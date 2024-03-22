08f7e4fcbfa9f904b96fe31e2baeffb1059fa7ed
Kiarash Astanboos <kiarash.astanboos@gmail.com>
2024-03-22 23:27:33 +0330

    init
    konane game /Base code

---
+++ Agent2.py
@@ -0,0 +1,70 @@
+from Tile import Tile
+
+class Agent2:
+
+    MIN_VALUE = -1000000
+    MAX_VALUE= 1000000
+
+    def __init__(self, game, color, max_depth):
+        self.game = game
+        self.color = color
+        self.max_depth = max_depth
+    
+
+    def do_min_max(self, current_board):
+        move, value = self.max(current_board, self.color, 0, self.MIN_VALUE, self.MAX_VALUE)
+ 
+        return move
+    
+
+    def max(self, current_board, current_color, depth, alpha, beta):
+       
+        if (self.game.check_terminal(current_board, current_color)):
+            return None, self.game.evaluate(current_board, current_color, -1000)
+
+        if (depth == self.max_depth):
+            return None, self.game.evaluate(current_board, current_color)
+
+        possible_moves =  self.game.generate_all_possible_moves(current_board, current_color)
+        best_move = None
+        best_move_value = self.MIN_VALUE
+        for move in possible_moves:
+            temp_move, value = self.min(current_board.next_board(current_color, move), self.game.opponent(current_color), depth + 1, alpha, beta)
+            if (value > best_move_value):
+                best_move_value = value
+                best_move = move
+                
+                if alpha < best_move_value:
+                    alpha = best_move_value
+                if beta <= best_move_value:
+                    return best_move, best_move_value
+                
+        return best_move, best_move_value
+
+   
+    def min(self, current_board, current_color, depth, alpha, beta):
+
+        if (self.game.check_terminal(current_board, current_color)):
+            return None, self.game.evaluate(current_board, current_color, 1000)
+        if (depth == self.max_depth):
+            return None, self.game.evaluate(current_board, current_color)
+
+        possible_moves =  self.game.generate_all_possible_moves(current_board, current_color)
+        best_move = None
+        best_move_value = self.MAX_VALUE
+        for move in possible_moves:
+            temp_move, value = self.max(current_board.next_board(current_color, move), self.game.opponent(current_color), depth + 1, alpha, beta)
+            if (value < best_move_value):
+                best_move_value = value
+                best_move = move
+                
+                if beta > best_move_value:
+                    beta = best_move_value
+                if best_move_value <= alpha:
+                    return best_move, best_move_value
+                
+        return best_move, best_move_value
+
+
+
+
---
+++ PlayGame.py
@@ -0,0 +1,32 @@
+from PlayKonane import PlayKonane
+from Tile import Tile
+from Agent import Agent
+from Agent2 import Agent2
+from Board import Board
+from KonaneGame2 import KonaneGame2
+from KonaneGame import KonaneGame
+
+
+class PlayGame:
+
+    def __init__(self):
+        NotImplemented
+
+    def play(self):
+        size = 6
+        game = KonaneGame()
+        game2 = KonaneGame2()
+        initial_board = Board(size, game.initialize_board(size))
+        agent1 = Agent(game, color=Tile.P_Black, max_depth=4)
+        agent2 = Agent(game2, color=Tile.P_White, max_depth=4)
+        # bot vs bot
+        play = PlayKonane(initial_board, game, agent1=agent1, agent2=agent2)
+
+        # player vs bot
+        #play = PlayKonane(initial_board, game, agent1=agent2)
+    
+
+
+
+if __name__ == '__main__':
+    PlayGame().play()
