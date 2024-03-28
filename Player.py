9356a91926f367dfc78b6591ad5afd1a15181f7c
Kiarash Astanboos <kiarash.astanboos@gmail.com>
2024-03-28 15:57:38 +0330

    dfs added

--- Player.py
+++ Player.py
@@ -9,6 +9,10 @@ class Player:
         self.name = name
         self.row = startingRow
         self.column = startingColumn
+        if self.name == 'player 1':
+            self.goalRow = 0
+        else:
+            self.goalRow = startingColumn * 2
 
     # TODO yek rah baraye residan be maqsad handle nashode!
     def getValidActions(self, board: Board) -> typing.List[
@@ -25,9 +29,41 @@ class Player:
                     if board.canPlaceWall(i, j, 'vertical'): available_actions.append(Action(i, j, 'vertical'))
                     if board.canPlaceWall(i, j, 'horizontal'): available_actions.append(Action(i, j, 'horizontal'))
         return available_actions
-    def doGo(self, board:Board,direction):
 
-        board.go(self,direction)
-    def doBuild(self,board,row,column,direction):
-        board.addWall(row,column,direction)
-        self.walls-=1
+    def doGo(self, board: Board, direction):
+
+        board.go(self, direction)
+
+    def doBuild(self, board: Board, row, column, direction):
+        board.addWall(row, column, direction)
+        self.walls -= 1
+
+    def checkForWall(self, row, column):  # for pathToOtherSide
+        if row == 2:
+            return (1, 0)
+        elif row == -2:
+            return (-1, 0)
+        elif column == -2:
+            return (0, -1)
+        elif column == 2:
+            return (0, 1)
+
+    def pathToOtherSide(self, board: Board) -> bool:
+        stack = [(self.row, self.column)]
+        visited = set()
+        while stack:
+            x, y = stack.pop()
+            if x == self.goalRow:
+                return True  # Player reached the goal
+            if (x, y) in visited:
+                continue  # Skip already visited nodes
+            visited.add((x, y))
+
+            # Check all adjacent cells
+            for dx, dy in [(2, 0), (-2, 0), (0, 2), (0, -2)]:
+                new_xW,new_yW=self.checkForWall(dx,dy)
+                new_x, new_y = x + dx, y + dy
+                if board.valid(new_x, new_y) and board[new_xW,new_yW]==-1:
+                    stack.append((new_x, new_y))  # Add legal moves to the stack
+
+        return False  # No path found
--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from Player import Player
 from Board import Board
 
-size = 5
+size = 9
 player2 = Player(10, 'player 2', 0, size - 1)
 player1 = Player(10, 'player 1', size * 2 - 2, size - 1)
 
 board = Board(size, player1, player2)
 # Board.display_board()
-board.addWall(1, 1, 'vertical')
-board.addWall(3, 3, 'horizontal')
-board.displayboard()
+# board.addWall(1, 1, 'vertical')
+# board.addWall(3, 3, 'horizontal')
+# board.displayboard()
 valid=player1.getValidActions(board)
 player1.doGo(board,'up')
+player1.doGo(board,'left')
+player2.doGo(board,'right')
+player1.doBuild(board,1,1,'vertical')
 board.displayboard()
 print (valid)
 
